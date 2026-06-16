396cd580842cf48abe5374fb1ebeec811c45c066
Haydar Oleyat <online@oleyat.no>
2026-06-16 12:25:15 +0200

    feat: added table subject & user

---
+++ backend/migrations/0002_create_tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS subject (
+    id UUID PRIMARY KEY DEFAULT uuidv7(),
+    content TEXT,
+    name TEXT NOT NULL,
+    code VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS user (
+    id UUID PRIMARY KEY DEFAULT uuidv7(),
+    sub UUID NOT NULL,
+    name TEXT NOT NULL,
+    email TEXT
+);
+
+DROP TABLE IF EXISTS test;
