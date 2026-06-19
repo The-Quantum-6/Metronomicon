da05997eb7f85c89b76f46ba1f16da3202f52f5c
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-06-19 14:18:19 +0200

    fix: various hadler compilation errors

--- backend/src/routes/courses/course_endpoint.rs
+++ backend/src/routes/courses/course_endpoint.rs
@@ -1,8 +1,9 @@
 use crate::models::course::Course;
 use crate::repositories::course as course_repo;
 use axum::extract::{Json, Path, State};
+use axum::http::StatusCode;
+use axum::{Router, routing::delete, routing::get, routing::post};
 use serde::Deserialize;
-use axum::{Router, extract::Query, routing::delete, routing::get, routing::post};
 use sqlx::PgPool;
 use uuid::Uuid;
 
@@ -22,33 +23,48 @@ struct CourseCreateRequest {
     code: String,
 }
 
-async fn get_courses(State(pool): State<PgPool>) -> Result<Vec<Course>, sqlx::Error> {
-    course_repo::get_courses(&pool).await
+async fn get_courses(State(pool): State<PgPool>) -> Result<Json<Vec<Course>>, StatusCode> {
+    course_repo::get_courses(&pool)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+        .map(|v| Json(v))
 }
 
 async fn create_course(
     State(pool): State<PgPool>,
     Json(course): Json<CourseCreateRequest>,
-) -> Result<(), sqlx::Error> {
-    course_repo::create_course(&pool, course.name, course.content, course.code).await
+) -> Result<(), StatusCode> {
+    course_repo::create_course(&pool, course.name, course.content, course.code)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
 }
+
 async fn get_course_by_id(
     State(pool): State<PgPool>,
     Path(id): Path<Uuid>,
-) -> Result<Json<Course>, sqlx::Error> {
-    Ok(Json(course_repo::get_course_by_id(&pool, id).await)?)
+) -> Result<Json<Course>, StatusCode> {
+    let course = course_repo::get_course_by_id(&pool, id)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        .ok_or(StatusCode::NOT_FOUND)?;
+
+    Ok(Json(course))
 }
 
 async fn get_course_by_code(
     State(pool): State<PgPool>,
     Path(code): Path<String>,
-) -> Result<Json<Course>, sqlx::Error> {
-    Ok(Json(course_repo::get_course_by_code(&pool, &code).await)?)
+) -> Result<Json<Course>, StatusCode> {
+    let course = course_repo::get_course_by_code(&pool, &code)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
+        .ok_or(StatusCode::NOT_FOUND)?;
+
+    Ok(Json(course))
 }
 
-async fn delete_course(
-    State(pool): State<PgPool>,
-    Path(id): Path<Uuid>,
-) -> Result<(), sqlx::Error> {
-    course_repo::delete_course(&pool, id).await
+async fn delete_course(State(pool): State<PgPool>, Path(id): Path<Uuid>) -> Result<(), StatusCode> {
+    course_repo::delete_course(&pool, id)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
 }
