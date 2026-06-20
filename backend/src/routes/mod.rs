2b4c39db72d5a8625c0cc5418cb8d16e882a0c06
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-06-20 13:35:57 +0200

    docs(routes): document routers

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -3,6 +3,7 @@ pub mod user;
 use axum::Router;
 use sqlx::PgPool;
 
+/// Main Router, merges domain-specific routers
 pub fn router() -> Router<PgPool> {
     Router::new().merge(user::router())
 }
--- backend/src/routes/user.rs
+++ backend/src/routes/user.rs
@@ -1,6 +1,7 @@
 use axum::{Router, routing::get};
 use sqlx::PgPool;
 
+/// Main user router. Manages routes relating to user management
 pub fn router() -> Router<PgPool> {
     Router::new().route("/testuser", get(user_test))
 }
