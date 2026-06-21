984ae45516b7582cd9b7b95a6c0987ca0b6add4d
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-06-21 20:54:24 +0200

    feat(courses): add create, read, and delete endpoints for courses

--- backend/src/routes/courses/course_endpoint.rs
+++ backend/src/routes/courses/course_endpoint.rs
@@ -1,7 +1,7 @@
+use crate::error::{AppError, RequestError};
 use crate::models::course::Course;
 use crate::repositories::course as course_repo;
 use axum::extract::{Json, Path, State};
-use axum::http::StatusCode;
 use axum::{Router, routing::delete, routing::get, routing::post};
 use serde::Deserialize;
 use sqlx::PgPool;
@@ -23,30 +23,24 @@ struct CourseCreateRequest {
     code: String,
 }
 
-async fn get_courses(State(pool): State<PgPool>) -> Result<Json<Vec<Course>>, StatusCode> {
-    course_repo::get_courses(&pool)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
-        .map(|v| Json(v))
+async fn get_courses(State(pool): State<PgPool>) -> Result<Json<Vec<Course>>, AppError> {
+    Ok(Json(course_repo::get_courses(&pool).await?))
 }
 
 async fn create_course(
     State(pool): State<PgPool>,
     Json(course): Json<CourseCreateRequest>,
-) -> Result<(), StatusCode> {
-    course_repo::create_course(&pool, course.name, course.content, course.code)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+) -> Result<(), AppError> {
+    Ok(course_repo::create_course(&pool, course.name, course.content, course.code).await?)
 }
 
 async fn get_course_by_id(
     State(pool): State<PgPool>,
     Path(id): Path<Uuid>,
-) -> Result<Json<Course>, StatusCode> {
+) -> Result<Json<Course>, AppError> {
     let course = course_repo::get_course_by_id(&pool, id)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
-        .ok_or(StatusCode::NOT_FOUND)?;
+        .await?
+        .ok_or(AppError::BadRequest(RequestError::NonExsistant("Course")))?;
 
     Ok(Json(course))
 }
@@ -54,17 +48,14 @@ async fn get_course_by_id(
 async fn get_course_by_code(
     State(pool): State<PgPool>,
     Path(code): Path<String>,
-) -> Result<Json<Course>, StatusCode> {
+) -> Result<Json<Course>, AppError> {
     let course = course_repo::get_course_by_code(&pool, &code)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
-        .ok_or(StatusCode::NOT_FOUND)?;
+        .await?
+        .ok_or(AppError::BadRequest(RequestError::NonExsistant("Course")))?;
 
     Ok(Json(course))
 }
 
-async fn delete_course(State(pool): State<PgPool>, Path(id): Path<Uuid>) -> Result<(), StatusCode> {
-    course_repo::delete_course(&pool, id)
-        .await
-        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)
+async fn delete_course(State(pool): State<PgPool>, Path(id): Path<Uuid>) -> Result<(), AppError> {
+    Ok(course_repo::delete_course(&pool, id).await?)
 }
