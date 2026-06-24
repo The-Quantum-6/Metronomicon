60eb056dccff83d66a53e7268c4271cb44b3a1c6
Haydar <online@oleyat.no>
2026-06-25 01:58:16 +0200

    fix: perms

--- backend/src/models/permissions.rs
+++ backend/src/models/permissions.rs
@@ -3,24 +3,24 @@ use bitflags::bitflags;
 bitflags! {
     pub struct Permissions: u32 {
         // reading
-        const READ = 1 << 3;
+        const READ = 1 << 1;
 
         // writing 
-        const WRITE_TEXT = 1 << 4;
-        const WRITE_FILE = 1 << 5;
+        const WRITE_TEXT = 1 << 2;
+        const WRITE_FILE = 1 << 3;
 
         // suggest changes
-        const SUGGEST_TEXT = 1 << 6;
-        const SUGGEST_FILE = 1 << 7;
+        const SUGGEST_TEXT = 1 << 4;
+        const SUGGEST_FILE = 1 << 5;
 
         // moderation
-        const MODERATE_TEXT = 1 << 8;
-        const MODERATE_FILE = 1 << 9;
+        const MODERATE_TEXT = 1 << 6;
+        const MODERATE_FILE = 1 << 7;
 
         // page management
-        const PAGE_ADMIN = 1 << 10;
+        const PAGE_ADMIN = 1 << 8;
 
         // permission transfer
-        const TRANSFER_PERMS = 1 << 11;
+        const TRANSFER_PERMS = 1 << 9;
     }
 }
