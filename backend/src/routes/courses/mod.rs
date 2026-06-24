d950f7333f9256824cc98d29e144fd6632d2128c
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-06-24 13:05:34 +0200

    chore(delete old): remove CRUD courses machinery

--- backend/src/routes/courses/mod.rs
+++
@@ -1,8 +0,0 @@
-pub mod course_endpoint;
-
-use axum::Router;
-use sqlx::PgPool;
-
-pub fn router() -> Router<PgPool> {
-    Router::new().merge(course_endpoint::router())
-}
--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -1,9 +1,8 @@
-pub mod courses;
 pub mod user;
 
 use axum::Router;
 use sqlx::PgPool;
 
 pub fn router() -> Router<PgPool> {
-    Router::new().merge(user::router()).merge(courses::router())
+    Router::new().merge(user::router())
 }
