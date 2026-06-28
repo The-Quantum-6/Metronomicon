7b646a0b614e78cf41fdfc10ab80615c0c91ee72
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-06-28 07:55:55 +0200

    migration!(optimize): use postgres jsonb

--- backend/migrations/0005_create_events.sql
+++ backend/migrations/0005_create_events.sql
@@ -5,8 +5,8 @@ CREATE TABLE events
     sequence       bigint CHECK (sequence >= 0) NOT NULL,
     event_type     text                         NOT NULL,
     event_version  text                         NOT NULL,
-    payload        json                         NOT NULL,
-    metadata       json                         NOT NULL,
+    payload        jsonb                         NOT NULL,
+    metadata       jsonb                         NOT NULL,
     PRIMARY KEY (aggregate_type, aggregate_id, sequence)
 );
 
@@ -14,6 +14,6 @@ CREATE TABLE course_query
 (
     view_id text                        NOT NULL,
     version           bigint CHECK (version >= 0) NOT NULL,
-    payload           json                        NOT NULL,
+    payload           jsonb                        NOT NULL,
     PRIMARY KEY (view_id)
 );
