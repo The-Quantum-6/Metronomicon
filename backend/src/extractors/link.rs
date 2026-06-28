d10afe83fbf486c090556224d822ff6c9d21342c
MrProgrammerMan <69722297+MrProgrammerMan@users.noreply.github.com>
2026-06-28 08:53:34 +0200

    feat(link): add command extractor

---
+++ backend/src/extractors/link.rs
@@ -0,0 +1,61 @@
+use axum::body::{Body, Bytes};
+use axum::extract::FromRequest;
+use axum::http::{Request, StatusCode};
+use axum::response::{IntoResponse, Response};
+use std::collections::HashMap;
+
+use crate::aggregates::link::command::LinkCommand;
+
+pub struct LinkCommandExtractor(pub HashMap<String, String>, pub LinkCommand);
+
+// const USER_AGENT_HDR: &str = "User-Agent";
+
+impl<S> FromRequest<S> for LinkCommandExtractor
+where
+    S: Send + Sync,
+{
+    type Rejection = LinkCommandExtractionError;
+
+    async fn from_request(req: Request<Body>, state: &S) -> Result<Self, Self::Rejection> {
+        let mut metadata = HashMap::default();
+        // Insert timestamp
+        metadata.insert("time".to_string(), chrono::Utc::now().to_rfc3339());
+        // metadata.insert("uri".to_string(), req.uri().to_string());
+        // if let Some(user_agent) = req.headers().get(USER_AGENT_HDR) {
+        //     if let Ok(value) = user_agent.to_str() {
+        //         metadata.insert(USER_AGENT_HDR.to_string(), value.to_string());
+        //     }
+        // }
+
+        // TODO: This may be the place attach the user id of the user that is issuing a command.
+        // We may want to create a higher level extractor that uses this one to collect those functionalities.
+
+        let body = Bytes::from_request(req, state).await?;
+        let command: LinkCommand = serde_json::from_slice(&body)?;
+        Ok(Self(metadata, command))
+    }
+}
+
+pub struct LinkCommandExtractionError;
+
+impl IntoResponse for LinkCommandExtractionError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::BAD_REQUEST,
+            "command could not be read".to_string(),
+        )
+            .into_response()
+    }
+}
+
+impl From<axum::extract::rejection::BytesRejection> for LinkCommandExtractionError {
+    fn from(_: axum::extract::rejection::BytesRejection) -> Self {
+        Self
+    }
+}
+
+impl From<serde_json::Error> for LinkCommandExtractionError {
+    fn from(_: serde_json::Error) -> Self {
+        Self
+    }
+}
