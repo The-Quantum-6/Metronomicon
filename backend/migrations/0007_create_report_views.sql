289f169cd1df54ac7ca68b870383f4e253557580
Bruker <bruker@Mac.lan>
2026-07-14 13:33:52 +0200

    feat(db): add report_list_view table

---
+++ backend/migrations/0007_create_report_views.sql
@@ -0,0 +1,7 @@
+CREATE TABLE report_list_view (
+    aggregate_id  TEXT NOT NULL,
+    target        TEXT NOT NULL,
+    description   TEXT NOT NULL,
+    contact_email TEXT,
+    status        TEXT NOT NULL
+);
