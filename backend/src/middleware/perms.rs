919f4a93fc634ac78132dd6106b7055a0cd31f86
Haydar <online@oleyat.no>
2026-07-15 00:23:31 +0200

    feat: perms methods

--- backend/src/middleware/perms.rs
+++ backend/src/middleware/perms.rs
@@ -10,6 +10,15 @@ pub fn can_transfer(perms: Permissions, requested: Permissions) -> bool {
     perms.contains(Permissions::TRANSFER_PERMS) && perms.contains(requested)
 }
 
+pub fn add_permission(perms: &mut Permissions, new_perm: Permissions) {
+    perms.insert(new_perm);
+}
+
+pub fn remove_permission(perms: &mut Permissions, perm_to_remove: Permissions) {
+    perms.remove(perm_to_remove);
+}
+
+
 pub fn is_super_user(role: &Role) -> bool {
     matches!(role, Role::Root | Role::SuperUser)
 }
