8237a9e7614ad19ab975125b8fee8e4f9abe847a
Haydar <online@oleyat.no>
2026-08-06 09:44:49 +0200

    fix: revert url to old

--- backend/src/auth/oidc.rs
+++ backend/src/auth/oidc.rs
@@ -243,7 +243,7 @@ pub async fn login_callback(
     session.insert("refresh_token", new_refresh).await.ok();
     session.insert("user_id", user.id).await.ok();
 
-    let mut response = Redirect::to("http://localhost:80/profile").into_response();
+    let mut response = Redirect::to("/").into_response();
     response
         .headers_mut()
         .insert(header::SET_COOKIE, HeaderValue::from_str(&cookie).unwrap());
